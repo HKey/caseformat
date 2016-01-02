72707c9f0f0819b4e2aa45876432a293aa07f814
HKey <s06139@gmail.com>
2016-01-02 21:30:56 +0900

    Add "Commentary" section

--- caseformat.el
+++ caseformat.el
@@ -23,7 +23,11 @@
 
 ;;; Commentary:
 
-;;
+;; This tool helps you to insert uppercase alphabetical characters without
+;; shift keys.
+
+;; Please see https://github.com/HKey/caseformat/blob/master/README.md for
+;; more details.
 
 ;;; Code:
 
