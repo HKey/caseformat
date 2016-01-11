97b61fbede348c654a195ac4d0a82811701feed8
HKey <s06139@gmail.com>
2016-01-12 00:19:46 +0900

    Exclude Emacs 24.2 and older from error on warn compilation

--- scripts/setup-error-on-warn.el
+++ scripts/setup-error-on-warn.el
@@ -1,3 +1,5 @@
 ;;; -*- lexical-binding: t; -*-
 
-(setq byte-compile-error-on-warn t)
+;; External cl-lib package causes byte compilation warnings
+(when (version<= "24.3" emacs-version)
+  (setq byte-compile-error-on-warn t))
